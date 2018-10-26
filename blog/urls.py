30440df8cba28ba2b565c7941ac2df9721e9533c
ClayAndMore <wangyusy@outlook.com>
2018-10-26 09:18:09 +0800

    增加左侧获得目录tree 接口

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
         url(r'^posts/(?P<post_class>[a-zA-Z]+)/(?P<post_type>[a-zA-Z0-9]+)/$', views.get_posts_list, name = 'posts_list'),
         url(r'^post/(?P<inum>[0-9]+)/$', views.get_post, name = 'post'),
         url(r'^archiving/$', views.archiving, name = 'archiving'),
+        url(r'^dir_list/$', views.dir_list),
         ]  
         
